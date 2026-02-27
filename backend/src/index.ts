ebfb56bae5db26b0e3bae5ae435cd4e282ea0b00
mwangaza12 <josephmwangaza1@gmail.com>
2026-02-27 18:02:07 +0300

    drizzle + neondb setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import type { Request, Response } from "express";
 import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 5000;
+
+const db = drizzle(process.env.DATABASE_URL!);
 
 app.get("/", (req: Request,res: Response) => {
     res.send("Support Facility API")
